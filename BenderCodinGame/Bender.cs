2447da1d799590f1e56612ba744c95ba6f2b5ec1
Hleb Kriukov <glebos05@gmail.com>
2021-02-13 15:13:14 +0200

    base

---
+++ BenderCodinGame/Bender.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenderCodinGame
+{
+    class Bender
+    {
+        public int positionX;
+        public int positionY;
+        public int wayvalue;
+
+        public Bender(int posX, int posY)
+        {
+            this.positionX = posX;
+            this.positionY = posY;
+            this.wayvalue = 0;
+        }
+
+        public void SetWay(char way)
+        {
+            switch (way)
+            {
+                case 'S':
+                    wayvalue = 0;
+                    break;
+                case 'E':
+                    wayvalue = 1;
+                    break;
+                case 'N':
+                    wayvalue = 2;
+                    break;
+                case 'W':
+                    wayvalue = 3;
+                    break;
+            }
+        }
+
+        public void SetNextWay()
+        {
+            if (this.wayvalue == 3)
+            {
+                this.wayvalue = 0;
+            }
+            else
+            {
+                this.wayvalue++;
+            }
+        }
+
+        public void GoInWay()
+        {
+            int way = this.wayvalue;
+            switch (way)
+            {
+                case 0:
+                    GoDown();
+                    break;
+                case 1:
+                    GoLeft();
+                    break;
+                case 2:
+                    GoUp();
+                    break;
+                case 3:
+                    GoRight();
+                    break;
+
+            }
+        }
+
+        public void GoDown()
+        {
+
+        }
+
+        public void GoLeft()
+        {
+
+        }
+
+        public void GoUp()
+        {
+
+        }
+
+        public void GoRight()
+        {
+
+        }
+    }
+}
---
+++ BenderCodinGame/MainClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BenderCodinGame
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int L = int.Parse(inputs[0]);
+            int C = int.Parse(inputs[1]);
+            List<string> map = new List<string>();
+            for (int i = 0; i < L; i++)
+            {
+                map.Add(Console.ReadLine());
+                map[i].Split("");
+            }
+
+            Bender bender = FindBender(map, "@");
+            Console.WriteLine(bender.positionX);
+        }
+
+        static Bender FindBender(List<string> map, string element)
+        {
+            for(int i = 0; i<map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == '@')
+                    {
+                        return new Bender(j, i);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
